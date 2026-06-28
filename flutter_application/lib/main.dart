15ffd85fdef1b5558eee62c48e54e24e23c37342
dovudbek7 <dovudbekm99@gmail.com>
2026-06-28 10:48:39 +0500

    #7 - Stateless Widgets & Hot Reload

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 // void main() {
@@ -9,37 +7,40 @@ import 'package:flutter/material.dart';
 // }
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Navbar',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-          backgroundColor: Color.fromARGB(255, 142, 18, 18),
-        ),
-        body: Center(
-          child: Text(
-            'Hello world',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-              color: Colors.green[500],
-              fontFamily: 'losevkaCharon',
-            ),
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Navbar',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => Text('data'),
-          backgroundColor: Color.fromARGB(255, 181, 28, 28),
-          child: Text('Click', style: TextStyle(color: Colors.white)),
+        backgroundColor: Color.fromARGB(255, 142, 18, 18),
+      ),
+      body: Center(
+        child: Text(
+          'Hello world',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.green[500],
+            fontFamily: 'losevkaCharon',
+          ),
         ),
       ),
-    ),
-  );
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Text('data'),
+        backgroundColor: Color.fromARGB(255, 181, 28, 28),
+        child: Text('Click', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
 }
