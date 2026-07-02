cd8a4a787671d63c18eab0a91bf6f5decfc11b65
dovudbek7 <dovudbekm99@gmail.com>
2026-07-02 15:29:20 +0500

    #18 - Custom Classes

--- quote/lib/main.dart
+++ quote/lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
-  runApp(MaterialApp(home: Quote()));
+  runApp(MaterialApp(home: QuoteList()));
 }
 
-class Quote extends StatefulWidget {
-  const Quote({super.key});
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
 
   @override
-  State<Quote> createState() => _QuoteState();
+  State<QuoteList> createState() => _QuoteListState();
 }
 
-class _QuoteState extends State<Quote> {
-  List<String> quotes = ['First', 'Second', 'Third'];
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+      author: 'Oscar Wilde',
+      text: 'Be yourself; everyone else is already taken.',
+    ),
+    Quote(
+      author: 'Albert Einstein',
+      text: 'Imagination is more important than knowledge.',
+    ),
+    Quote(author: 'Steve Jobs', text: 'Stay hungry, stay foolish.'),
+    Quote(
+      author: 'Nelson Mandela',
+      text: 'It always seems impossible until it\'s done.',
+    ),
+    Quote(
+      author: 'Walt Disney',
+      text:
+          'All our dreams can come true, if we have the courage to pursue them.',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +40,21 @@ class _QuoteState extends State<Quote> {
         title: Text('Navbar', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.red[600],
       ),
-      body: Column(children: quotes.map((quote) => Text(quote)).toList()),
+      body: Column(
+        children: quotes
+            .map(
+              (quote) => Container(
+                child: Column(
+                  children: [
+                    Text(quote.author),
+                    SizedBox(height: 10),
+                    SizedBox(width: 300, child: Text(quote.text)),
+                  ],
+                ),
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
